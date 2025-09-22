80cf0e1eb8212006f645dcd0727f0817cdaf4583
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-09-21 22:38:23 -0400

    Tested code in main

--- main.py
+++ main.py
@@ -8,3 +8,40 @@
 # resolution to model basic geometric shapes                                            #
 #########################################################################################
 
+from Circle import Circle
+from Rectangle import Rectangle
+from Square import Square
+
+shapes = [
+    Circle(10,10,4,"Circle_1"),
+    Circle(20,30,9,"Circle_2"),
+    Rectangle(10,20,"Rectangle_1"),
+    Rectangle(20,30,"Rectangle_2"),
+    Square(10,"Square")
+    ]
+
+print("--- Polymorphism check ---")
+for shape in shapes:
+    print(f"{shape.name} Area = {shape.are:.5f}")
+
+print("--- Getter/setter check ---")
+
+# Circle
+circle = shapes[0]
+print(f"{circle.name} Current: {circle.radius} {circle.area:.5f}")
+circle.radius = circle.radius * 2
+print(f"{circle.name} Doubled: {circle.radius} {circle.area:.5f}")
+
+# Rectangle
+rectangle = shapes[2]
+print(f"{rectangle.name} Current: {rectangle.length} {rectangle.width} {rectangle.area:.5f}")
+rectangle._length = rectangle.length * 2
+rectangle._width = rectangle.width * 2
+rectangle.calc_area()
+print(f"{rectangle.name} Doubled: {rectangle.length} {rectangle.width} {rectangle.area:.5f}")
+
+# Square
+square = shapes[4]
+print(f"{square.name} Current: {square.side} {square.area:.5f}")
+square.side = square.side * 2
+print(f"{square.name} Doubled: {square.side} {square.area:.5f}")
